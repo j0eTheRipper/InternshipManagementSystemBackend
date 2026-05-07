5b9ca286bcaa631380884d9c8b49723812a5418c
j0eTheRipper <j0etheripper0010@gmail.com>
2026-05-07 17:58:37 +0800

    #2: created the test for the models

---
+++ tests/app/login/test_models.py
@@ -0,0 +1,24 @@
+from app.login.dbExceptions import IncorrectEmailOrPassword
+from app.login.models import User
+import pytest
+
+
+def test_user_login(mocker):
+    mock_user = [("mock", "mockname", "mockmail@mail.com", "mockPass", "student")]
+    mocker.patch(
+        "app.dependencies.database_connector.execute_read", return_value=mock_user
+    )
+
+    assert (
+        User.login("correctMail@mail.com", "correctPassword")
+        != IncorrectEmailOrPassword
+    )
+
+
+def test_user_login_wrong_creds(mocker):
+    mock_user = []
+    mocker.patch(
+        "app.dependencies.database_connector.execute_read", return_value=mock_user
+    )
+    with pytest.raises(IncorrectEmailOrPassword):
+        User.login("wrongEmail@email.email", "wrongPassword")
